6a57603c9deb6288eb030bdca78b28b83a463f9c
akbaycb <akbaycb@gmail.com>
2024-06-27 22:46:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,4 @@ if ingredients_list:
 
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
     fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-    st.text(fruityvice_response)
 
